0b03df13f1ca4e052e71db4064a12b693840206d
dsanderson90 <dsanderson3@cnm.edu>
2019-04-22 23:09:41 -0600

    first commit w cordova

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import { render } from "react-dom";
+import { ReactDOM } from "react-dom";
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById("root"));
+const startApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+    registerServiceWorker();
+  };
+  
+  if(window.cordova) {
+    document.addEventListener('deviceready', startApp, false);
+  } else {
+    startApp();
+  }
